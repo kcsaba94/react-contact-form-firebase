2d2bf9cea819fe3e81f83756566536c6af52e9f9
kcsaba94 <k.csaba994@gmail.com>
2022-08-25 18:53:35 +0200

    async and UI is completed

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -17,34 +17,57 @@ const ContactForm = (props) => {
 
   const [submitted, setSubmitted] = useState(false);
 
+  async function addDataHandler(user) {
+    const response = await fetch(
+      "https://react-http-d591e-default-rtdb.europe-west1.firebasedatabase.app/contacts.json",
+      {
+        method: "POST",
+        body: JSON.stringify(user),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    const data = await response.json();
+    console.log(data);
+  }
+
   const onSubmitHandler = (event) => {
     event.preventDefault();
 
     if (name.length === 0 || /\d/.test(name)) {
-        setErrorName(true);
-        return;
+      setErrorName(true);
+      return;
     } else {
-        setErrorName(false);
+      setErrorName(false);
     }
     if (email.trim().length === 0) {
-        setErrorEmail(true);
-        return;
+      setErrorEmail(true);
+      return;
     } else {
-        setErrorEmail(false);
+      setErrorEmail(false);
     }
     if (message.trim().length === 0) {
-        setErrorMessage(true);
-        return;
+      setErrorMessage(true);
+      return;
     } else {
-        setErrorMessage(false);
+      setErrorMessage(false);
     }
 
-    setName('');
-    setEmail('');
-    setMessage('');
+    setName("");
+    setEmail("");
+    setMessage("");
 
     setSubmitted(true);
 
+    const user = {
+      uName: name,
+      uEmail: email,
+      uMessage: message,
+    };
+
+    addDataHandler(user);
   };
 
   const onChangeNameHandler = (event) => {
@@ -59,49 +82,64 @@ const ContactForm = (props) => {
     setEmail(event.target.value);
   };
 
-  const submittedMessage = 'The form is submitted!';
+  const submittedMessage = "The form is submitted!";
+
+  const returnForm = () => {
+    setSubmitted(false);
+  };
 
   return (
-    <form className={classes["contact-form"]} onSubmit={onSubmitHandler}>
-      <h2>Contact Us</h2>
-      <Label id='nameId' text='Full name'/>
-      <Input
-        type="text"
-        class="form-control"
-        placeholder="Full name"
-        onChange={onChangeNameHandler}
-        value={name}
-        id='nameId'
-        maxlength="40"
-        errorName={errorName}
-      />
-        <Label id='emailId' text='E-mail'/>
-        <Input
-        type="email"
-        class="form-control"
-        placeholder="E-mail"
-        onChange={onChangeEmailHandler}
-        value={email}
-        id='emailId'
-        maxlength="40"
-        errorEmail={errorEmail}
-      />
-      <Label id='messageId' text='Message'/>
-      <Input
-        type="text"
-        class="form-control"
-        placeholder="Message"
-        onChange={onChangeMessageHandler}
-        value={message}
-        id='messageId'
-        maxlength="100"
-        errorMessage={errorMessage}
-      />
-      <Button class="btn btn-primary" type="submit" text="Send Message" />
-      <div style={{paddingTop: '10px'}}>
-        <span style={{color: 'green', fontWeight: '500'}}>{submitted && submittedMessage}</span>
+    <div className={classes["contact-form"]}>
+      {!submitted && (
+        <form onSubmit={onSubmitHandler}>
+          <h2>Contact Us</h2>
+          <Label id="nameId" text="Full name" />
+          <Input
+            type="text"
+            class="form-control"
+            placeholder="Full name"
+            onChange={onChangeNameHandler}
+            value={name}
+            id="nameId"
+            maxlength="40"
+            errorName={errorName}
+          />
+          <Label id="emailId" text="E-mail" />
+          <Input
+            type="email"
+            class="form-control"
+            placeholder="E-mail"
+            onChange={onChangeEmailHandler}
+            value={email}
+            id="emailId"
+            maxlength="40"
+            errorEmail={errorEmail}
+          />
+          <Label id="messageId" text="Message" />
+          <Input
+            type="text"
+            class="form-control"
+            placeholder="Message"
+            onChange={onChangeMessageHandler}
+            value={message}
+            id="messageId"
+            maxlength="100"
+            errorMessage={errorMessage}
+          />
+          <Button class="btn btn-primary" type="submit" text="Send Message" />
+        </form>
+      )}
+      <div style={{ paddingTop: "10px" }}>
+        <span style={{ color: "green", fontWeight: "500" }}>
+          {submitted && submittedMessage}
+        </span>
       </div>
-    </form>
+      {submitted && (
+        <button onClick={returnForm} className="btn btn-secondary">
+          Back
+        </button>
+      )}
+    </div>
   );
 };
 
